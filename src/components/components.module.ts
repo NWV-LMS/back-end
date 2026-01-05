9605d96d963134f5fc0dc60a80bc84aae07f87e8
abduvalee95 <abduvalee95@gmail.com>
2026-01-05 21:40:17 +0600

    feat: create lead,& student,course,group modules

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { LeadModule } from './lead/lead.module';
+import { Student,course,groupModule } from './student,course,group/student,course,group.module';
+import { StudentModule } from './student/student.module';
+import { CourseModule } from './course/course.module';
+import { GrouModule } from './grou/grou.module';
+import { GroupModule } from './group/group.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, LeadModule, Student,course,groupModule, StudentModule, CourseModule, GrouModule, GroupModule],
   exports: [AuthModule, UserModule]
 })
 export class ComponentsModule {}
