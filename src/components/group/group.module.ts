a94b6ef02bec96cd9ca46292012aa113594ce985
abduvalee95 <abduvalee95@gmail.com>
2026-01-07 22:25:37 +0600

    feat(group): register group module

--- src/components/group/group.module.ts
+++ src/components/group/group.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { GroupController } from './group.controller';
+import { GroupService } from './group.service';
 
-@Module({})
+@Module({
+  controllers: [GroupController],
+  providers: [GroupService],
+  exports: [GroupService],
+})
 export class GroupModule {}
